1f13efd753f1d3ef0cb2e782559fcbfa77447d6d
Francis-sanjon <francis.s@annulartechnologies.com>
2022-10-15 13:06:51 +0530

    commit-002

--- cypress/e2e/TC_001_Login.cy.js
+++ cypress/e2e/TC_001_Login.cy.js
@@ -4,11 +4,10 @@ const { default: Login } = require("./PageObjects/Login.spec")
 const lp = new Login();
 let i = 0;
 
-describe('Login', () => {
+describe('', () => {
 
   Given('User launch the Adactin webpage', () => {
     cy.visit('https://adactinhotelapp.com/')
-
   })
   When('User enters valid Username and valid password', () => {
     cy.fixture('xlsxData').then((data) => {
@@ -23,25 +22,26 @@ describe('Login', () => {
   Then('User able to Adactin Homepage', () => {
     cy.title().should('eq', 'Adactin.com - Search Hotel')
   })
-
-})
-describe('', () => {
-
   When('User enters invalid Username and valid password', () => {
-    lp.userName(data.rows[i].username)
-    lp.password('Francis*123')
+    cy.fixture('xlsxData').then((data) => {
+      lp.userName(data.rows[i].Invalid_username)
+      lp.password(data.rows[i].password)
+    })
   })
   Then('User unable to Adactin Homepage', () => {
-    cy.title().should('eq', 'Adactin.com - Search Hotel')
+    cy.title().should('not.eq', 'Adactin.com - Search Hotel')
   })
   When('User enters valid Username and invalid password', () => {
-    lp.userName('Francis3535')
-    lp.password('Francis*123')
+    cy.fixture('xlsxData').then((data) => {
+      lp.userName(data.rows[i].username)
+      lp.password(data.rows[i].Invalid_password)
+    })
   })
   When('User enters invalid Username and invalid password', () => {
-    lp.userName('Francis3535')
-    lp.password('Francis*123')
-    //lp.password('Francis*123')
-  })
+    cy.fixture('xlsxData').then((data) => {
+      lp.userName(data.rows[i].Invalid_username)
+      lp.password(data.rows[i].Invalid_password)
+    })
 
+  })
 })
